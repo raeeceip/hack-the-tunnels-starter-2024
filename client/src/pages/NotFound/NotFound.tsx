098d224177596bf8ba1eb14c1884fd5c23ea9589
toluwalasewasehere <haastrup.toluwalase@gmail.com>
2024-10-19 13:55:10 -0400

    Updated Timetable Naming for Challenge 2

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,15 +4,10 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-<<<<<<< HEAD
-      <h1>404</h1>
-      <img src = "404 Page-Featured-Image" alt = "404 Image"></img>
-=======
       <div className="layout">
         <h1>404</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
       </div>
->>>>>>> ed59159506afb3d7a355db057b985f85822a3f6d
     </Layout>
   );
 }
