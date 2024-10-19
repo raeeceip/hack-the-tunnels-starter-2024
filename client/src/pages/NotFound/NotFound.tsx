393958f201c75663cf3629a95f0d64c6fea96cb3
Chiso <chiboguchisomu@gmail.com>
2024-10-19 15:58:23 +0000

    update login page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,10 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <div className="layout">
+        <h1>404</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
     </Layout>
   );
 }
